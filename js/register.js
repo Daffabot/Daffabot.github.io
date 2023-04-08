e14dc9be92e53065c52327090276eeb3a9c15b8e
Daffabot <96946212+Daffabot@users.noreply.github.com>
2023-04-08 20:05:24 +0700

    Update register.js

--- js/register.js
+++ js/register.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-navigator.serviceWorker.register('/service-worker.js').
+navigator.serviceWorker.register('../js/service-worker.js').
     then(function (registration) {
         // Registration was successful``
         console.log('ServiceWorker registration successful with scope: ', 
