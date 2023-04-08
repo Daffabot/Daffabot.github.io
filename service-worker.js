f08ef117efbff6a6840fffd41aa846d795e3758a
Daffabot <96946212+Daffabot@users.noreply.github.com>
2023-04-08 20:51:01 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,52 +1,31 @@
-// membuat fitur pwa
-const cacheName = "v1";
-// buat variable untuk asset yg ingin di cache
-const cacheAssets = [
-  "./index.html",
-  "./css/stylefooter.css",
-  "./css/style1.css",
-  "./css/index.css",
-  "./manifest.json"
-  "./service-worker.js",
-  "./js/register.js",
-  "./image/favicon.ico",
-];
-
-// memanggil event install
-self.addEventListener("install", (e) => {
-  console.log("Service Worker: Installed");
-
-  e.waitUntil(
-    caches
-      .open(cacheName)
-      .then((cache) => {
-        console.log("Service Worker: Caching Files");
-        cache.addAll(cacheAssets);
-      })
-      .then(() => self.skipWaiting())
-  );
+self.addEventListener('install', e => {
+ e.waitUntil(
+    caches.open('airhorner').then(cache => {
+        return cache.addAll([
+            '/',
+            '/?utm_source=homescreen',
+            '/index.html',
+            '/css/stylefooter.css',
+            '/css/style1.css',
+            '/css/index.css',
+            '/manifest.json',
+            '/js/register.js',
+            '/js/service-worker.js',
+            '/image/favicon.ico'
+        ])
+            .then(() => self.skipWaiting());
+    })
+  )
 });
 
-// memanggil event activate untuk aktivasi
-self.addEventListener("activate", (e) => {
-  console.log("Service Worker: Activated");
-  // menghapus cache jika nama sama
-  e.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== cacheName) {
-            console.log("Service Worker: Clearing Old Cache");
-            return caches.delete(cache);
-          }
-        })
-      );
+self.addEventListener('activate', event => {
+ event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+        return response || fetch(event.request);
     })
   );
-});
-
-// Call Fetch Event
-self.addEventListener("fetch", (e) => {
-  console.log("Service Worker: Fetching");
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
