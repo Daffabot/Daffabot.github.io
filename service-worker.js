fbb488af8d0645b827ec65f5a6750f374bd695f6
Daffabot <daffabot@programmer.net>
2023-05-26 15:16:19 +0700

    Update service-worker.js bener

--- service-worker.js
+++ service-worker.js
@@ -18,10 +18,13 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    }).catch(() => {
-      return caches.match('index.html');
-    })
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+      .catch(() => {
+        return fetch(event.request)
+          .catch(() => caches.match('index.html'));
+      })
   );
 });
