e21c0cbd562b2e2ea9ccca749c4826bd26c3df4e
Daffabot <daffabot@programmer.net>
2023-05-26 15:10:18 +0700

    Update serviceWorker.js new

--- js/serviceWorker.js
+++ js/serviceWorker.js
@@ -133,3 +133,13 @@ export function unregister() {
     });
   }
 }
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    }).catch(() => {
+      return caches.match('index.html');
+    })
+  );
+});
