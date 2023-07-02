9746466f76a4dfbe62490e8794a501266ee9393a
Daffabot <daffabot@programmer.net>
2023-07-02 08:53:33 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -84,16 +84,29 @@ self.addEventListener("activate", (e) => {
   console.log("Service Worker: Activated");
   // menghapus cache jika nama sama
   e.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== cacheName) {
-            console.log("Service Worker: Clearing Old Cache");
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
+    caches
+      .open(cacheName)
+      .then((cache) => {
+        console.log("Service Worker: Caching Files");
+        return Promise.all(
+          cacheAssets.map((url) => {
+            return fetch(url)
+              .then((response) => {
+                if (!response.ok) {
+                  throw new Error(`Request failed for ${url}`);
+                }
+                return cache.put(url, response);
+              })
+              .catch((error) => {
+                console.error("Failed to cache", url, error);
+              });
+          })
+        );
+      })
+      .then(() => {
+        console.log("Service Worker: Caching Files Completed");
+        return self.skipWaiting();
+      })
   );
 });
 
