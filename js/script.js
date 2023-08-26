ee1b3b760e92e9969f8428316b76667a58b61366
Daffabot <daffabot@programmer.net>
2023-08-26 20:17:36 +0700

    Clean Code For script.js

--- js/script.js
+++ js/script.js
@@ -1,31 +1,31 @@
 //Script to preload image header + loading
-    function preloadImage(url) {
-      return new Promise((resolve, reject) => {
-        const img = new Image();
-        img.src = url;
-        img.onload = resolve;
-        img.onerror = reject;
-      });
-    }
+function preloadImage(url) {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.src = url;
+    img.onload = resolve;
+    img.onerror = reject;
+  });
+}
 
-    window.onload = () => {
-      var myVar;
-      function myFunction() {
-        myVar = setTimeout(showPage, 3000);
-      }
-      function showPage() {
-        document.getElementById("loader").style.display = "none";
-        document.getElementById("myDiv").style.display = "block";
-      }
-      myFunction();
-      preloadImage('../image/profil.png')
-        .then(() => {
-          console.log('Gambar profil berhasil dimuat pertama');
-        })
-        .catch((error) => {
-          console.error('Gagal memuat gambar profil:', error);
-        });
-    };
+window.onload = () => {
+  var myVar;
+  function myFunction() {
+    myVar = setTimeout(showPage, 3000);
+  }
+  function showPage() {
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("myDiv").style.display = "block";
+  }
+  myFunction();
+  preloadImage("../image/profil.png")
+    .then(() => {
+      console.log("Gambar profil berhasil dimuat pertama");
+    })
+    .catch((error) => {
+      console.error("Gagal memuat gambar profil:", error);
+    });
+};
 
 //Script To Reveal Content
 function reveal() {
@@ -46,13 +46,14 @@ function reveal() {
 
 window.addEventListener("scroll", reveal);
 
-
 //Script For Up To Scroll
 // Get the button
 let mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -69,13 +70,11 @@ function topFunction() {
 }
 
 //script for comments disqus
-disqusLoader( '.disqustret',
-	{
-		scriptUrl:		'//daffabot-main.disqus.com/embed.js',
-		disqusConfig:	function()
-		{
-			this.page.identifier 	= 'Daffabot';
-			this.page.url			= 'https://www.daffabot.my.id';
-			this.page.title			= 'Portofolio Daffa Ahmad Ibrahim';
-		}
-	});
+disqusLoader(".disqustret", {
+  scriptUrl: "//daffabot-main.disqus.com/embed.js",
+  disqusConfig: function () {
+    this.page.identifier = "Daffabot";
+    this.page.url = "https://www.daffabot.my.id";
+    this.page.title = "Portofolio Daffa Ahmad Ibrahim";
+  },
+});
